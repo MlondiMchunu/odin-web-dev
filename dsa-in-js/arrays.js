9dd381aff9bb32deac4c40b8d88563515677db9a
MlondiMchunu <mlondiemchunu1@gmail.com>
2024-11-01 10:34:02 +0200

    latest commits

--- dsa-in-js/arrays.js
+++ dsa-in-js/arrays.js
@@ -1,3 +1,12 @@
+Array.prototype.average = function(){
+    //calculate sum
+    var sum = this.reduce(function(prev,cur){
+        return prev + cur
+    })
+    //return sum divided by number of elements
+    return sum / this.length
+}
+
 var arr = [1,2,3,4,5]
 var avg = arr.average(arr)
 console.log(avg)
